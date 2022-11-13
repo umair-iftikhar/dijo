595db6dd93f384b6da35100d1ff28e68bacbb174
Umair Iftikhar <umair.umi@live.co.uk>
2022-11-14 03:21:55 +0500

    application v1 is ready to release

--- app/dijo.py
+++ app/dijo.py
@@ -26,6 +26,25 @@ def new_file():
             writer.writerows(mydict)
 
 
+
+####################################################
+# Validate an entry #
+####################################################
+
+def validate_entry(en):
+    
+    # define log type (log_type)
+    if en[0].lower() == "n" or en[0].lower() == "t" or en[0].lower() == "e" or en[0].lower() == "d" or en[0]=="!": 
+        return True
+    else:
+        return False
+
+
+
+
+
+
+
 ####################################################
 # Convert string into CSV Columns and return Dict. #
 ####################################################
@@ -36,16 +55,17 @@ def create_entry(en):
     dict = {"date_time": now}
     
     # define log type (log_type)
-    if en[0].lower() == "n" or en[0].lower() == "t" or en[0].lower() == "e" or en[0].lower() == "d" or en[0]=="!": 
+    if validate_entry(en): 
         dict["log_type"] = en[0].upper()
     else:
-        return "Invalid entry"
+        raise Exception("String is not following the guideline. Please refer Guide.")
 
     # define log (log)
     dict["log"] = en[1:].replace(" ", "", 1)
 
     # define project name (project)
-    dict["project"] = en.rsplit(" @")[1].split(" ")[0]
+    if "@" in en:
+        dict["project"] = en.rsplit(" @")[1].split(" ")[0]
 
     return dict
 
